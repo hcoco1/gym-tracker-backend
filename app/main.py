fe0dcec846f1970f72d1ff4951378a4649fa8b33
hcoco1 <arias.ivan@gmail.com>
2025-05-06 21:43:45 -0400

    cors updated

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://gym-tracker-backend-3jsb.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
